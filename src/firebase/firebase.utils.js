1224ba67b0b89f9c23646dc32e5da825d7dba2f5
Matt Chang <mattchangnyc@gmail.com>
2020-11-02 14:06:31 -0500

    integrated firebase; sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBngZQtIJMcbipP29vBT-6N2ZbFUkn_cfY",
+  authDomain: "crwn-db-581da.firebaseapp.com",
+  databaseURL: "https://crwn-db-581da.firebaseio.com",
+  projectId: "crwn-db-581da",
+  storageBucket: "crwn-db-581da.appspot.com",
+  messagingSenderId: "265104203215",
+  appId: "1:265104203215:web:3b37d17ef695011f6786ba",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
