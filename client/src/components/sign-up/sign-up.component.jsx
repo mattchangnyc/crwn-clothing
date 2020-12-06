c78a2fb84261f82932a03798473b28021b060073
Matt Chang <mattchangnyc@gmail.com>
2020-12-06 12:37:30 -0500

    fix sign-up.component

--- client/src/components/sign-up/sign-up.component.jsx
+++ client/src/components/sign-up/sign-up.component.jsx
@@ -6,7 +6,8 @@ import CustomButton from '../custom-button/custom-button.component';
 
 // import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss';
+// import './sign-up.styles.scss';
+import { SignUpContainer, SignUpTitle } from './sign-up.styles';
 
 import { signUpStart }from '../../redux/user/user.actions';
 
@@ -72,8 +73,8 @@ const SignUp = ({ signUpStart}) => {
     // render() {
         // const { displayName, email, password, confirmPassword } = this.state;
         return(
-            <div className='sign-up'>
-                <h2 className='title'>I do not have a account</h2>
+            <SignUpContainer>
+                <SignUpTitle>I do not have a account</SignUpTitle>
                 <span>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={handleSubmit}>
                     <FormInput
@@ -110,7 +111,7 @@ const SignUp = ({ signUpStart}) => {
                     />  
                     <CustomButton type='submit'>SIGN UP</CustomButton>                  
                 </form>
-            </div>
+            </SignUpContainer>
         )
     // }
 }
