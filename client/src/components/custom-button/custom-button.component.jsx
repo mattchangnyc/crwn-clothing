02e317862f7c9c2b6bf14a4b01356546ccb119d1
Matt Chang <mattchangnyc@gmail.com>
2020-12-06 12:10:38 -0500

    fix cart-dropdown.component

--- client/src/components/custom-button/custom-button.component.jsx
+++ client/src/components/custom-button/custom-button.component.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
-//import { CustomButtonContainer } from './custom-button.styles';
+// import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
-// const CustomButton = ({ children, ...props}) => (
-//     <CustomButtonContainer  {...props}>
-//         {children}
-//     </CustomButtonContainer>
-// );
+ const CustomButton = ({ children, ...props}) => (
+     <CustomButtonContainer  {...props}>
+         {children}
+     </CustomButtonContainer>
+ );
 
-const CustomButton = ({
-    children,
-    isGoogleSignIn,
-    inverted,
-    ...otherProps
-  }) => (
-    <button
-      className={`${inverted ? 'inverted' : ''} ${
-        isGoogleSignIn ? 'google-sign-in' : ''
-      } custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+// const CustomButton = ({
+//     children,
+//     isGoogleSignIn,
+//     inverted,
+//     ...otherProps
+//   }) => (
+//     <button
+//       className={`${inverted ? 'inverted' : ''} ${
+//         isGoogleSignIn ? 'google-sign-in' : ''
+//       } custom-button`}
+//       {...otherProps}
+//     >
+//       {children}
+//     </button>
+//   );
 
 export default CustomButton;
--- client/src/components/custom-button/custom-button.styles.jsx
+++ client/src/components/custom-button/custom-button.styles.jsx
@@ -12,35 +12,34 @@ const buttonStyles = css`
   }
 `;
 
-
 const invertedButtonStyles = css`
-    background-color: white;
-    color: black;
-    border: 1px solid black;
+  background-color: white;
+  color: black;
+  border: 1px solid black;
 
-    &:hover {
-      background-color: black;
-      color: white;
-      border: none;
-    }
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+  }
 `;
 
 const googleSignInStyles = css`
-    background-color: #4285f4;
-    color: white;
+  background-color: #4285f4;
+  color: white;
 
-    &:hover {
-      background-color: #357ae8;
-      border: none;
-    }
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+  }
 `;
 
 const getButtonStyles = props => {
-    if (props.isGoogleSignIn) {
-        return googleSignInStyles;
-    }
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
 
-    return props.inverted ? invertedButtonStyles : buttonStyles;
+  return props.inverted ? invertedButtonStyles : buttonStyles;
 };
 
 export const CustomButtonContainer = styled.button`
@@ -52,11 +51,10 @@ export const CustomButtonContainer = styled.button`
   padding: 0 35px 0 35px;
   font-size: 15px;
   text-transform: uppercase;
-  font-family: "Open Sans Condensed";
+  font-family: 'Open Sans Condensed';
   font-weight: bolder;
   cursor: pointer;
   display: flex;
   justify-content: center;
-
   ${getButtonStyles}
 `;
